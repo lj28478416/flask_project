2e8dd8ee44e804c2854c423f949b706eba8470de
liujie <lj28478416>
2018-05-24 22:43:03 +0800

    delete_dailfresh

--- dailyfresh/config.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.102.133:3306/ticket'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-DEBUG = True
--- dailyfresh/model.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db =SQLAlchemy()
-class Ticket(db.Model):
-    __tablename__ = 'ticket'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20), nullable=False)
-    name_en = db.Column(db.String(20), nullable=False)
